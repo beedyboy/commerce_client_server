a79e0782902a18fd92dd9ba630b54be7bf23b78a
Bolade <boladebode@gmail.com>
2020-08-14 14:03:09 +0100

    latest update

--- models/auth.js
+++ models/auth.js
@@ -221,7 +221,7 @@ try {
 				const payload = db(tbl).where('id', data[id]).select().then(payload => {
 				 if (tbl === "sellers") {
 				  // res.cookie('access_token', token, {httpOnly: true}).json({
-					 res.cookie('access_token', token).json({
+					 res.cookie('access_token', token, { sameSite: true }).json({
 					status: 200,
 					payload,
 					token,
